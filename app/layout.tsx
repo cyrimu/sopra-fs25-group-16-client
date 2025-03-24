7aaadc7aec5225c8f35e7c81aa5e335e42f42dfb
Rashmi Dindgur <85095918+rashmidindgur@users.noreply.github.com>
2025-03-24 12:10:27 +0100

    customize tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Student XX-XXX-XXX",
+  title: "Codenames",
   description: "sopra-fs25-template-client",
 };
 
