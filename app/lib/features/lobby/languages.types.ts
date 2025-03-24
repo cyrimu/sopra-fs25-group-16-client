6431290709fa9ac35b2eb8b510c55846d8c3da5d
sgm17 <sergigm78@gmail.com>
2025-03-24 15:53:18 +0100

    implement lobby feature

---
+++ app/lib/features/lobby/languages.types.ts
@@ -0,0 +1,5 @@
+export enum LANGUAGES {
+  english,
+  german,
+  spanish,
+}
---
+++ app/lib/features/lobby/lobby.types.ts
@@ -0,0 +1,16 @@
+import { Game, GAME_TYPE } from "../game/game.types";
+import { Player } from "../player/player.types";
+import { LANGUAGES } from "./languages.types";
+import { Team } from "./team.types";
+
+export interface Lobby {
+  lobbyId: number | undefined;
+  host: string | undefined;
+  players: Player[];
+  game: Game | undefined;
+  type: GAME_TYPE;
+  language: LANGUAGES;
+  blueTeam: Team | undefined;
+  redTeam: Team | undefined;
+  connections: number;
+}
