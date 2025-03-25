9ef5144b60197361f6aff1843d5fb4819395719a
sgm17 <sergigm78@gmail.com>
2025-03-25 16:33:36 +0100

    create lobby screen

---
+++ app/lobby/[id]/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import "@ant-design/v5-patch-for-react-19";
+import { useSelector } from "react-redux";
+import { selectPlayerName } from "@/lib/features/player";
+import {
+  selectGameType,
+  selectLanguage,
+  selectLobbyId,
+} from "@/lib/features/lobby";
+import { LANGUAGES } from "@/lib/features/lobby/languages.types";
+import { GAME_TYPE } from "@/lib/features/game/game.types";
+
+export default function Lobby() {
+  const lobbyId = useSelector(selectLobbyId);
+  const playerName = useSelector(selectPlayerName);
+  const language = useSelector(selectLanguage);
+  const type = useSelector(selectGameType);
+
+  return (
+    <div style={{ color: "white" }}>
+      <div>lobbyId: {lobbyId}</div>
+      <div>playerName: {playerName}</div>
+      <div>language: {LANGUAGES[language]}</div>
+      <div>type: {GAME_TYPE[type]}</div>
+    </div>
+  );
+}
