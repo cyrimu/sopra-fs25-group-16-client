f9a3b2fbc81bb0ee4eee9a3b1584d843e1a6df7a
sgm17 <sergigm78@gmail.com>
2025-04-02 16:01:22 +0200

    implement the game screen with its styles

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -3,55 +3,97 @@ import "@ant-design/v5-patch-for-react-19";
 import styles from "@/styles/game.module.css";
 import LogButton from "@/components/logButton";
 import GameCard from "@/components/gameCard";
-import { GAME_TYPE } from "@/lib/features/game/game.types";
-import { CARD_COLOR } from "@/lib/features/game/card.types";
+import { useState } from "react";
+import LogDialog from "@/components/logDialog";
+import { useSelector } from "react-redux";
+import { selectCards } from "@/lib/features/game";
+import SubmitButton from "@/components/submitButton";
 
 export default function Game() {
-  const colorCards = Array.from({ length: 16 }).map((e, i) => {
-    return {
-      type: GAME_TYPE.text,
-      content: i % 2 === 0 ? "BLUE" : "RED",
-      isRevealed: false,
-      color: i % 2 === 0 ? CARD_COLOR.blue : CARD_COLOR.red,
-    };
-  });
+  const [isLog, setIsLog] = useState(false);
 
-  const greyCards = Array.from({ length: 8 }).map((e, i) => {
-    return {
-      type: GAME_TYPE.text,
-      content: `GREY`,
-      isRevealed: false,
-      color: CARD_COLOR.grey,
-    };
-  });
-
-  const blackCard = {
-    type: GAME_TYPE.text,
-    content: "BLACK",
-    isRevealed: false,
-    color: CARD_COLOR.black,
-  };
-
-  const cards = [...colorCards, ...greyCards, blackCard];
-  const shuffledCards = cards.sort(() => Math.random() - 0.5);
+  const cards = useSelector(selectCards);
 
   return (
     <div className={styles.centered}>
       <div style={{ background: "#A79C8B", width: "100%", height: "100%" }}>
-        <LogButton />
-        <div className={styles.gameWrapper}>
-          <div className={styles.gameContainer}>
-            {shuffledCards.map((e, i) => {
-              return (
-                <GameCard
-                  key={i}
-                  type={e.type}
-                  content={e.content}
-                  isRevealed={e.isRevealed}
-                  color={e.color}
-                />
-              );
-            })}
+        {!isLog && <LogButton callback={() => setIsLog(true)} />}
+        {isLog && <LogDialog callback={() => setIsLog(false)} />}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div className={styles.gameWrapper}>
+            <div className={styles.gameContainer}>
+              {cards.map((e, i) => {
+                return (
+                  <GameCard
+                    key={i}
+                    type={e.type}
+                    content={e.content}
+                    isRevealed={e.isRevealed}
+                    color={e.color}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <SubmitButton />
+          <div className={styles.scoreboardWrapper}>
+            <div
+              className={styles.scoreboardContainer}
+              style={{ backgroundColor: "#651C25", paddingRight: "150px" }}
+            >
+              <div className={styles.scoreboardContent}>
+                <div className={styles.scoreboardTeamContainer}>
+                  <span>Operatives</span>
+                  <div className={styles.scoreboardPlayersContainer}>
+                    {Array.from({ length: 2 }).map((_, i) => (
+                      <div className={styles.scoreboardPlayer} key={i}>
+                        <span>Player {i + 1}</span>
+                        <span>0</span>
+                      </div>
+                    ))}
+                  </div>
+                  <span>Spymaster</span>
+                  <div className={styles.scoreboardPlayersContainer}>
+                    {Array.from({ length: 2 }).map((_, i) => (
+                      <div className={styles.scoreboardPlayer} key={i}>
+                        <span>Player {i + 1}</span>
+                        <span>0</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <span className={styles.scoreboardScore}>8</span>
+              </div>
+            </div>
+            <div
+              className={styles.scoreboardContainer}
+              style={{
+                backgroundColor: "#294C61",
+                paddingLeft: "150px",
+              }}
+            >
+              <div className={styles.scoreboardContent}>
+                <span className={styles.scoreboardScore}>8</span>
+                <div className={styles.scoreboardTeamContainer}>
+                  <span>Operatives</span>
+                  <div className={styles.scoreboardPlayersContainer}>
+                    {Array.from({ length: 2 }).map((_, i) => (
+                      <div className={styles.scoreboardPlayer} key={i}>
+                        <span>Player {i + 1}</span>
+                        <span>0</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
