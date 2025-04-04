990fe7db740dd70554feb6f4fdb0a6a65d35345e
sgm17 <sergigm78@gmail.com>
2025-04-04 21:34:05 +0200

    animate the card and add the image card type

--- app/components/gameCard.tsx
+++ app/components/gameCard.tsx
@@ -1,22 +1,47 @@
 import { Card, CARD_COLOR } from "@/lib/features/game/card.types";
 import { GAME_TYPE } from "@/lib/features/game/game.types";
+import { selectPlayer } from "@/lib/features/player";
 import styles from "@/styles/game.module.css";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const gameCard: React.FC<Card> = ({ type, color, content }) => {
-  if (type === GAME_TYPE.text) {
+  const { role } = useSelector(selectPlayer);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  if (type !== GAME_TYPE.text) {
     return (
-      <div
+      <motion.div
         className={styles.gameCard}
         style={
           {
-            "--bg-image": `url("/${CARD_COLOR[color]}_card.png")`,
+            "--bg-image": isFlipped
+              ? `url("/grey_card.png")`
+              : `url("/${CARD_COLOR[color]}_card.png")`,
           } as React.CSSProperties
         }
+        animate={{ rotateY: isFlipped ? 180 : 0 }}
+        transition={{ duration: 0.6, ease: "easeInOut" }}
+        onClick={() => setIsFlipped(!isFlipped)}
       >
-        <div className={styles.gameCardTextContainer}>
-          <span>{content.toUpperCase()}</span>
-        </div>
-      </div>
+        {!isFlipped && (
+          <div className={styles.gameCardTextContainer}>
+            <span>{content.toUpperCase()}</span>
+          </div>
+        )}
+      </motion.div>
+    );
+  } else {
+    return (
+      <motion.div
+        className={styles.gameCard}
+        animate={{ rotateY: isFlipped ? 180 : 0 }}
+        transition={{ duration: 0.6, ease: "easeInOut" }}
+        onClick={() => setIsFlipped(!isFlipped)}
+      >
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Zebra_%2824694097565%29.jpg/1200px-Zebra_%2824694097565%29.jpg" />
+      </motion.div>
     );
   }
 };
