b61e5e62b134c570451bd69c63ebc8ad157826f7
sgm17 <sergigm78@gmail.com>
2025-04-04 21:50:06 +0200

    add card selected state

--- app/components/gameCard.tsx
+++ app/components/gameCard.tsx
@@ -1,31 +1,41 @@
 import { Card, CARD_COLOR } from "@/lib/features/game/card.types";
 import { GAME_TYPE } from "@/lib/features/game/game.types";
 import { selectPlayer } from "@/lib/features/player";
+import { PLAYER_ROLES } from "@/lib/features/player/player.types";
 import styles from "@/styles/game.module.css";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
-const gameCard: React.FC<Card> = ({ type, color, content }) => {
+const gameCard: React.FC<Card> = ({ type, color, content, isRevealed }) => {
   const { role } = useSelector(selectPlayer);
-  const [isFlipped, setIsFlipped] = useState(false);
+  const [revealed, setRevealed] = useState(isRevealed);
+  const [selected, setSelected] = useState(false);
 
-  if (type !== GAME_TYPE.text) {
+  function determineBackgroundImage(): CARD_COLOR {
+    if (role === PLAYER_ROLES.operative && !revealed) {
+      // The only case where the card will be forced to grey
+      return CARD_COLOR.grey;
+    }
+    return color;
+  }
+
+  const cardColor = determineBackgroundImage();
+
+  if (type === GAME_TYPE.text) {
     return (
       <motion.div
-        className={styles.gameCard}
+        className={selected ? styles.gameCardSelected : styles.gameCard}
         style={
           {
-            "--bg-image": isFlipped
-              ? `url("/grey_card.png")`
-              : `url("/${CARD_COLOR[color]}_card.png")`,
+            "--bg-image": `url("/${CARD_COLOR[cardColor]}_card.png")`,
           } as React.CSSProperties
         }
-        animate={{ rotateY: isFlipped ? 180 : 0 }}
+        animate={{ rotateY: revealed ? 180 : 0 }}
         transition={{ duration: 0.6, ease: "easeInOut" }}
-        onClick={() => setIsFlipped(!isFlipped)}
+        onClick={() => setSelected(!selected)}
       >
-        {!isFlipped && (
+        {!revealed && (
           <div className={styles.gameCardTextContainer}>
             <span>{content.toUpperCase()}</span>
           </div>
@@ -36,9 +46,9 @@ const gameCard: React.FC<Card> = ({ type, color, content }) => {
     return (
       <motion.div
         className={styles.gameCard}
-        animate={{ rotateY: isFlipped ? 180 : 0 }}
+        animate={{ rotateY: revealed ? 180 : 0 }}
         transition={{ duration: 0.6, ease: "easeInOut" }}
-        onClick={() => setIsFlipped(!isFlipped)}
+        onClick={() => setRevealed(!revealed)}
       >
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Zebra_%2824694097565%29.jpg/1200px-Zebra_%2824694097565%29.jpg" />
       </motion.div>
