d32e88671081a8f58da7c2e605a7b90073e7d338
Rashmi Dindgur <85095918+rashmidindgur@users.noreply.github.com>
2025-04-06 18:31:18 +0200

    customize selection style

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,67 +7,76 @@ import RulesButton from "@/components/rulesButton";
 import StoreProvider from "./providers/StoreProvider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Codenames",
-  description: "sopra-fs25-template-client",
+    title: "Codenames",
+    description: "sopra-fs25-template-client",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    return (
+        <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ConfigProvider
-          theme={{
-            algorithm: theme.defaultAlgorithm,
-            token: {
-              // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
-              colorPrimary: "#22426b", // selected input field boarder will have this color as well
-              borderRadius: 8,
-              colorText: "#fff",
-              fontSize: 16,
+            theme={{
+                algorithm: theme.defaultAlgorithm,
+                token: {
+                    // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
+                    colorPrimary: "#22426b", // selected input field boarder will have this color as well
+                    borderRadius: 8,
+                    colorText: "black",
+                    fontSize: 16,
+                    fontFamily: "Gabarito",
 
-              // Alias Token
-              colorBgContainer: "#16181D",
-            },
-            // if a component type needs special styling, setting here will override default options set in token
-            components: {
-              Button: {
-                colorPrimary: "rgba(217, 217, 217, 0.5)",
-                algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
-                controlHeight: 38,
-              },
-              Input: {
-                colorBorder: "gray", // color boarder selected is not overridden but instead is set by primary color in line 35
-                colorTextPlaceholder: "#888888",
-                algorithm: false, // disable algorithm (line 32)
-              },
-              Form: {
-                labelColor: "#fff",
-                algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
-              },
-              Card: {},
-            },
-          }}
+                    // Alias Token
+                    colorBgContainer: "#16181D",
+
+                },
+                // if a component type needs special styling, setting here will override default options set in token
+                components: {
+                    Button: {
+                        colorPrimary: "rgba(217, 217, 217, 0.5)",
+                        algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
+                        controlHeight: 38,
+                    },
+                    Input: {
+                        colorBorder: "gray", // color boarder selected is not overridden but instead is set by primary color in line 35
+                        colorTextPlaceholder: "#888888",
+                        algorithm: false, // disable algorithm (line 32)
+                    },
+                    Form: {
+                        labelColor: "#fff",
+                        algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
+                    },
+                    Card: {},
+                    Select: {
+                        activeBorderColor	: "white",
+                        selectorBg: "white",
+                        colorTextPlaceholder: "black",
+                        optionSelectedBg: "#2f2f2f",
+                        optionSelectedColor: "white",
+                    },
+                },
+            }}
         >
-          <AntdRegistry>
-            <StoreProvider>{children}</StoreProvider>
-            <RulesButton type="primary" iconType="question" />
-          </AntdRegistry>
+            <AntdRegistry>
+                <StoreProvider>{children}</StoreProvider>
+                <RulesButton type="primary" iconType="question" />
+            </AntdRegistry>
         </ConfigProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
