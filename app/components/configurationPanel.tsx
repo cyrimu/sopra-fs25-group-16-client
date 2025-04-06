f088489dc90c205179c0e283e52e9f8998d0f698
Rashmi Dindgur <85095918+rashmidindgur@users.noreply.github.com>
2025-04-06 17:15:32 +0200

    get ready screen implemented

--- app/components/configurationPanel.tsx
+++ app/components/configurationPanel.tsx
@@ -1,8 +1,8 @@
-import React, { } from 'react';
+import React from 'react';
 import {Form, Radio, Select} from 'antd';
 import {GAME_TYPE} from "@/lib/features/game/game.types";
-import { LANGUAGES } from "@/lib/features/lobby/languages.types";
-
+import {LANGUAGES} from "@/lib/features/lobby/languages.types";
+import styles from "@/styles/page.module.css";
 
 const ConfigurationPanel: React.FC = () => {
 
@@ -22,10 +22,12 @@ const ConfigurationPanel: React.FC = () => {
         <div>
             <Form layout="vertical">
                 <Form.Item label="Game Type">
-                    <Radio.Group value={gameType} onChange={handleGameTypeChange}>
-                        <Radio.Button value={GAME_TYPE.text}>Text</Radio.Button>
-                        <Radio.Button value={GAME_TYPE.image}>Image</Radio.Button>
-                    </Radio.Group>
+                    <div style={{textAlign: 'center'}}>
+                       <Radio.Group value={gameType} onChange={handleGameTypeChange} style={{width: '100%'}}>
+                           <Radio.Button value={GAME_TYPE.text} style={{width: '50%', color: gameType === GAME_TYPE.text ? 'black' : 'white', backgroundColor: gameType === GAME_TYPE.text ? 'white' : 'black'}}>Word</Radio.Button>
+                           <Radio.Button value={GAME_TYPE.image} style={{width: '50%', color: gameType === GAME_TYPE.image ? 'black' : 'white', backgroundColor: gameType === GAME_TYPE.image ? 'white' : 'black'}}>Picture</Radio.Button>
+                       </Radio.Group>
+                    </div>
                 </Form.Item>
                 {gameType === GAME_TYPE.text && (
                     <Form.Item label="Language">
@@ -38,42 +40,46 @@ const ConfigurationPanel: React.FC = () => {
                         </Select>
                     </Form.Item>
                 )}
-                <Form.Item label="Blue Spy Master">
-                    <Select value={language} onChange={handleLanguageChange}>
-                        {Object.values(LANGUAGES).map((lang) => (
-                            <Select.Option key={lang} value={lang}>
-                                {lang}
-                            </Select.Option>
-                        ))}
-                    </Select>
-                </Form.Item>
-                <Form.Item label="Blue Operative">
-                    <Select value={language} onChange={handleLanguageChange}>
-                        {Object.values(LANGUAGES).map((lang) => (
-                            <Select.Option key={lang} value={lang}>
-                                {lang}
-                            </Select.Option>
-                        ))}
-                    </Select>
-                </Form.Item>
-                <Form.Item label="Red Spy Master">
-                    <Select value={language} onChange={handleLanguageChange}>
-                        {Object.values(LANGUAGES).map((lang) => (
-                            <Select.Option key={lang} value={lang}>
-                                {lang}
-                            </Select.Option>
-                        ))}
-                    </Select>
-                </Form.Item>
-                <Form.Item label="Red Operative">
-                    <Select value={language} onChange={handleLanguageChange}>
-                        {Object.values(LANGUAGES).map((lang) => (
-                            <Select.Option key={lang} value={lang}>
-                                {lang}
-                            </Select.Option>
-                        ))}
-                    </Select>
-                </Form.Item>
+                <div className={styles.formRow}>
+                    <Form.Item label="Blue Spymaster" className={styles.formItem}>
+                        <Select value={language} onChange={handleLanguageChange}>
+                            {Object.values(LANGUAGES).map((lang) => (
+                                <Select.Option key={lang} value={lang}>
+                                    {lang}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="Blue Spymaster" className={styles.formItem}>
+                        <Select value={language} onChange={handleLanguageChange}>
+                            {Object.values(LANGUAGES).map((lang) => (
+                                <Select.Option key={lang} value={lang}>
+                                    {lang}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+                </div>
+                <div className={styles.formRow}>
+                    <Form.Item label="Blue Spymaster" className={styles.formItem}>
+                        <Select value={language} onChange={handleLanguageChange}>
+                            {Object.values(LANGUAGES).map((lang) => (
+                                <Select.Option key={lang} value={lang}>
+                                    {lang}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="Blue Spymaster" className={styles.formItem}>
+                        <Select value={language} onChange={handleLanguageChange}>
+                            {Object.values(LANGUAGES).map((lang) => (
+                                <Select.Option key={lang} value={lang}>
+                                    {lang}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+                </div>
             </Form>
         </div>
     );
---
+++ app/components/getReady.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '@/styles/page.module.css';
+
+const GetReadyScreen: React.FC = () => {
+    return (
+        <div className={styles.centered}>
+            <div className={styles.redBlueOverlay}></div>
+            <div className={styles.messageField}>
+                Get ready, your game is starting. <br />
+                <br />
+                You are on your own now.
+                <br />
+                <br />- CN
+            </div>
+        </div>
+    );
+}
+
+export default GetReadyScreen;
