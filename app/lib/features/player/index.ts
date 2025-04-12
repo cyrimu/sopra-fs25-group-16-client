9a9d0a3a99bf0abcffeafcddbd3a2b74235e0217
sgm17 <sergigm78@gmail.com>
2025-04-12 19:46:34 +0200

    update the player state as the only thing needed is the playerName

--- app/lib/features/player/index.ts
+++ app/lib/features/player/index.ts
@@ -1,13 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { Player, PLAYER_ROLES } from "./player.types";
-import { TEAM_COLOR } from "../lobby/team.types";
 
-const initialState = {
+interface PlayerState {
+  playerName: string | undefined;
+}
+
+const initialState: PlayerState = {
   playerName: undefined,
-  role: undefined,
-  team: undefined,
-} satisfies Player as Player;
+};
 
 const playerSlice = createSlice({
   name: "player",
@@ -16,21 +16,14 @@ const playerSlice = createSlice({
     setPlayerName(state, action: PayloadAction<string>) {
       state.playerName = action.payload;
     },
-    setPlayerRole(state, action: PayloadAction<PLAYER_ROLES>) {
-      state.role = action.payload;
-    },
-    setPlayerTeam(state, action: PayloadAction<TEAM_COLOR>) {
-      state.team = action.payload;
-    },
   },
   selectors: {
-    selectPlayer: (state) => state,
+    selectPlayerName: ({ playerName }) => playerName,
   },
 });
 
-export const { setPlayerName, setPlayerRole, setPlayerTeam } =
-  playerSlice.actions;
+export const { setPlayerName } = playerSlice.actions;
 
-export const { selectPlayer } = playerSlice.selectors;
+export const { selectPlayerName } = playerSlice.selectors;
 
 export default playerSlice.reducer;
