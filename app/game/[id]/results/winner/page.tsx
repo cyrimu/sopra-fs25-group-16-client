388b8831c0b9083575a7dacd6779258626e708a3
Calvin Koch <calvin.koch@uzh.ch>
2025-04-13 21:27:37 +0200

    minor fix winner screen

--- app/game/[id]/results/winner/page.tsx
+++ app/game/[id]/results/winner/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 export default function Lobby() {
     // temporary until websocket connection
     const router = useRouter();
-    const isHost = false;
+    const isHost = true;
     const team = "blue";
     const gameId = "abc123";
     const lobbyId = "abc123";
