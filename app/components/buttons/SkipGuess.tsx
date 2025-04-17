af5ca3ddccb7c77f63da25e8a7ffb36410dc9d06
sgm17 <sergigm78@gmail.com>
2025-04-17 07:41:23 +0200

    create the skip guess component to send a skip guess message to the backend

---
+++ app/components/buttons/SkipGuess.tsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./SkipGuess.module.css";
+import { selectPlayerName } from "@/lib/features/player";
+
+const SkipGuess: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const username = useSelector(selectPlayerName);
+
+  function submitSelectionHandle() {
+    dispatch({
+      type: "socket/skipGuess",
+      payload: username,
+    });
+  }
+
+  return (
+    <button className={styles.skipGuessButton} onClick={submitSelectionHandle}>
+      Skip Guess
+    </button>
+  );
+};
+
+export default SkipGuess;
