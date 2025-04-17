61ed65dc3cd969e1400ee91cadd046586a2d03d0
sgm17 <sergigm78@gmail.com>
2025-04-17 07:51:10 +0200

    display the logs inside the log dialog

--- app/components/logDialog.tsx
+++ app/components/logDialog.tsx
@@ -1,5 +1,7 @@
+import { useSelector } from "react-redux";
 import styles from "./LogDialog.module.css";
 import { CloseOutlined } from "@ant-design/icons";
+import { selectLogs } from "@/lib/features/game";
 
 interface LogDialogProps {
   callback: () => void;
@@ -10,10 +12,26 @@ const LogDialog: React.FC<LogDialogProps> = ({ callback }) => {
     callback();
   }
 
+  const logs = useSelector(selectLogs);
+
   return (
     <div className={styles.logWrapper}>
       <div className={styles.logClose} onClick={handleClose}>
         <CloseOutlined />
+        <ul
+          style={{
+            paddingTop: "20px",
+            fontSize: 16,
+            textAlign: "center",
+            listStyleType: "none",
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          {logs?.map((l, index) => (
+            <li key={index}>{l}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
