32a0d208358930b8460fac34374e4016d00b838e
sgm17 <sergigm78@gmail.com>
2025-04-23 16:43:02 +0200

    ensure that the operative does not select more cards than the provided number as clue

--- app/components/gameCard.tsx
+++ app/components/gameCard.tsx
@@ -6,10 +6,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { motion } from "framer-motion";
 import { useMemo } from "react";
 import { selectPlayerName } from "@/lib/features/player";
-import { selectPlayers } from "@/lib/features/game";
+import {
+  selectLogs,
+  selectPlayers,
+  selectSelectedCards,
+} from "@/lib/features/game";
 import { setSelectedCard } from "@/lib/features/game";
 import Image from "next/image";
-
 
 interface GameCardProps {
   card: Card;
@@ -25,10 +28,20 @@ const GameCard: React.FC<GameCardProps> = ({ card, selected }) => {
 
   const role = players?.find((e) => e.playerName === playerName)?.role;
 
+  const selectedCards = useSelector(selectSelectedCards);
+
   const animation = useMemo(
     () => ({ rotateY: isRevealed ? 180 : 0 }),
     [isRevealed]
   );
+
+  const logs = useSelector(selectLogs);
+
+  function lastProvidedClue(): number {
+    const lastClue = logs?.findLast((e) => e.includes("provided the Clue"));
+    const numArr = lastClue!.split(" : ");
+    return Number(numArr[numArr.length - 1]);
+  }
 
   function determineBackgroundImage(): CARD_COLOR {
     if (
@@ -51,42 +64,49 @@ const GameCard: React.FC<GameCardProps> = ({ card, selected }) => {
     )
       return;
 
-    dispatch(setSelectedCard(card.id));
+    // Number of selected cars < clue
+    const providedClue = lastProvidedClue();
+    if (selectSelectedCards.length < providedClue) {
+      dispatch(setSelectedCard(card.id));
+    }
   }
 
   if (type !== GAME_TYPE.TEXT) {
     return (
-        <motion.div
-            className={styles.card}
-            animate={{rotateY: isRevealed ? 180 : 0}}
-            transition={{duration: 0.6, ease: "easeInOut"}}
-        >
-          <Image
-              src={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Zebra_%2824694097565%29.jpg/1200px-Zebra_%2824694097565%29.jpg"}
-              alt=""/>
-        </motion.div>
+      <motion.div
+        className={styles.card}
+        animate={{ rotateY: isRevealed ? 180 : 0 }}
+        transition={{ duration: 0.6, ease: "easeInOut" }}
+      >
+        <Image
+          src={
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Zebra_%2824694097565%29.jpg/1200px-Zebra_%2824694097565%29.jpg"
+          }
+          alt=""
+        />
+      </motion.div>
     );
   } else {
     return (
-        <motion.div
-            className={selected ? styles.cardSelected : styles.card}
-            style={
-              {
-                "--bg-image": `url("/${CARD_COLOR[
-                    cardColor
-                    ].toLowerCase()}_card.png")`,
-              } as React.CSSProperties
-            }
-            animate={animation}
-            transition={{duration: 0.6, ease: "easeInOut"}}
-            onClick={handleSelectCard}
-        >
-          {!isRevealed && (
-              <div className={styles.cardTextContainer}>
-                <span>{content.toUpperCase()}</span>
-              </div>
-          )}
-        </motion.div>
+      <motion.div
+        className={selected ? styles.cardSelected : styles.card}
+        style={
+          {
+            "--bg-image": `url("/${CARD_COLOR[
+              cardColor
+            ].toLowerCase()}_card.png")`,
+          } as React.CSSProperties
+        }
+        animate={animation}
+        transition={{ duration: 0.6, ease: "easeInOut" }}
+        onClick={handleSelectCard}
+      >
+        {!isRevealed && (
+          <div className={styles.cardTextContainer}>
+            <span>{content.toUpperCase()}</span>
+          </div>
+        )}
+      </motion.div>
     );
   }
 };
