e7945fd551bc0d49fc74ca00aa49039fcadd1529
sgm17 <sergigm78@gmail.com>
2025-05-06 20:52:05 +0200

    create the function to save the current game

--- app/components/buttons/SaveButton.tsx
+++ app/components/buttons/SaveButton.tsx
@@ -3,13 +3,58 @@ import React from "react";
 import { Button as AntButton, Popconfirm } from "antd";
 import styles from "./SaveButton.module.css";
 import { SaveOutlined } from "@ant-design/icons";
-import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "@/lib/store";
+import {
+  selectGameId,
+  selectGameTypeFromGame,
+  selectLanguageFromGame,
+  selectPlayers,
+} from "@/lib/features/game";
+import { Old } from "@/lib/features/old/old.types";
+
+const OLD_IDS_KEY = "OLD_IDS_KEY";
 
 const SaveButton: React.FC = () => {
-  const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
 
-  function handleSaveLobby() {
-    router.back();
+  const gameId = useSelector(selectGameId);
+  const players = useSelector(selectPlayers);
+  const gameType = useSelector(selectGameTypeFromGame);
+  const language = useSelector(selectLanguageFromGame);
+
+  function handleSaveGame() {
+    // Save the current game inside the local storage
+    saveCurrentGame();
+    // Send the save action to the other players
+    dispatch({
+      type: "game/saveAction",
+    });
+  }
+
+  function saveCurrentGame() {
+    if (!gameId) throw new Error("The gameId is undefined");
+    if (!players) throw new Error("The players are undefined");
+    if (!gameType) throw new Error("The gameType is undefined");
+    if (!language) throw new Error("The language is undefined");
+
+    // Fetch old games
+    const oldIds = localStorage.getItem(OLD_IDS_KEY)?.split(",") ?? [];
+    // Insert the new game id
+    oldIds.push(gameId);
+    // Store the old game ids with the current one
+    const uniqueIds = Array.from(new Set(oldIds));
+    localStorage.setItem(OLD_IDS_KEY, uniqueIds.join(","));
+
+    // Create the oldGame object
+    const oldGame: Old = {
+      gameID: gameId,
+      players: players,
+      type: gameType,
+      language: language,
+    };
+    // Set the old game to the local storage
+    localStorage.setItem(gameId, JSON.stringify(oldGame));
   }
 
   return (
@@ -20,7 +65,7 @@ const SaveButton: React.FC = () => {
             Do you want to quit and save the lobby?
           </span>
         }
-        onConfirm={handleSaveLobby}
+        onConfirm={handleSaveGame}
         okText="Yes"
         cancelText="No"
         icon={false}
