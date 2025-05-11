7356581eb9423d9e8facb66c9760e104484c5dee
Rashmi Dindgur <85095918+rashmidindgur@users.noreply.github.com>
2025-05-12 00:52:36 +0200

    derive results

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ConfigProvider, theme } from "antd";
+import {ConfigProvider, theme} from "antd";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "@/styles/globals.css";
 import RulesButton from "@/components/buttons/RulesButton";
@@ -67,6 +67,18 @@ export default function RootLayout({
                 optionSelectedBg: "#2f2f2f",
                 optionSelectedColor: "white",
               },
+              InputNumber:{
+                colorBorder: "gray",
+                colorTextPlaceholder: "#888888",
+                algorithm: true,
+                filledHandleBg: "white",
+                addonBg: "white",
+                colorText: "black",
+                colorTextDescription: "black",
+                colorTextHeading: "black",
+                colorTextLabel: "black",
+
+              }
             },
           }}
         >
