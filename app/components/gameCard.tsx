9e78290a1c55d6a81e7340435827abb23cd6a10f
Calvin Koch <calvin.koch@uzh.ch>
2025-05-13 17:40:12 +0200

    rebased with main

--- app/components/gameCard.tsx
+++ app/components/gameCard.tsx
@@ -7,6 +7,7 @@ import React, { useMemo } from "react";
 import { setSelectedCard } from "@/lib/features/game";
 import Image from "next/image";
 import { selectMyPlayerInGame } from "../../utils/helpers";
+import { GAME_TYPE } from "@/lib/features/game/game.types";
 
 interface GameCardProps {
   card: Card;
@@ -15,7 +16,7 @@ interface GameCardProps {
 
 const GameCard: React.FC<GameCardProps> = ({ card, selected }) => {
   const dispatch = useDispatch();
-  const { color, content, isRevealed } = card;
+  const { color, content, isRevealed, type } = card;
 
   const myPlayerInGame = useSelector(selectMyPlayerInGame);
   const role = myPlayerInGame?.role;
@@ -34,13 +35,8 @@ const GameCard: React.FC<GameCardProps> = ({ card, selected }) => {
   const shouldRenderText = shouldShowContent && !isImage;
 
   function determineBackgroundImage(): CARD_COLOR {
-    const role = myPlayerInGame?.role;
-
-    //if (!role) throw new Error("The role of the player in game is null");
-
     if (
-      (role === PLAYER_ROLES.BLUE_OPERATIVE ||
-        role === PLAYER_ROLES.RED_OPERATIVE) &&
+      (role === PLAYER_ROLES.BLUE_OPERATIVE || role === PLAYER_ROLES.RED_OPERATIVE) &&
       !isRevealed
     ) {
       return CARD_COLOR.WHITE;
@@ -52,52 +48,12 @@ const GameCard: React.FC<GameCardProps> = ({ card, selected }) => {
 
   function handleSelectCard() {
     if (!role) throw new Error("The role of the player in game is null");
-
     if (role === PLAYER_ROLES.RED_SPYMASTER || role === PLAYER_ROLES.BLUE_SPYMASTER)
       return;
 
     dispatch(setSelectedCard(card.id));
   }
 
-  if (type !== GAME_TYPE.TEXT) {
-    return (
-      <motion.div
-        className={styles.card}
-        animate={{ rotateY: isRevealed ? 180 : 0 }}
-        transition={{ duration: 0.6, ease: "easeInOut" }}
-      >
-        <Image
-          src="/test.jpg"
-          alt="Test Image"
-          width={300}
-          height={300}
-          style={{ objectFit: "cover", width: "100%", height: "100%" }}
-        />
-      </motion.div>
-    );
-  } else {
-    return (
-      <motion.div
-        className={selected ? styles.cardSelected : styles.card}
-        style={
-          {
-            "--bg-image": `url("/${CARD_COLOR[
-              cardColor
-            ].toLowerCase()}_card.png")`,
-          } as React.CSSProperties
-        }
-        animate={animation}
-        transition={{ duration: 0.6, ease: "easeInOut" }}
-        onClick={handleSelectCard}
-      >
-        {!isRevealed && (
-          <div className={styles.cardTextContainer}>
-            <span>{content.toUpperCase()}</span>
-          </div>
-        )}
-      </motion.div>
-    );
-  }
   return (
     <motion.div
       className={selected ? styles.cardSelected : styles.card}
@@ -106,7 +62,7 @@ const GameCard: React.FC<GameCardProps> = ({ card, selected }) => {
           "--bg-image": isRevealed || isSpymaster
             ? `url("/${CARD_COLOR[cardColor].toLowerCase()}_card.png")`
             : `url("/white_card.png")`,
-          aspectRatio: isImage ? "1 / 1" : "2.15",
+          aspectRatio: type === GAME_TYPE.PICTURE ? "1 / 1" : "2.15",
         } as React.CSSProperties
       }
       animate={animation}
