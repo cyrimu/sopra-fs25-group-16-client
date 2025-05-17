419f69b266830e9b3ac134d209c186ac64d5ba0b
sgm17 <sergigm78@gmail.com>
2025-05-17 21:18:06 +0200

    implement the logic to return to the lobby screen after visualizing the results

--- app/results/[id]/page.tsx
+++ app/results/[id]/page.tsx
@@ -1,15 +1,23 @@
 "use client";
-import { selectWinner } from "@/lib/features/game";
-import styles from "@/styles/page.module.css";
-import { useRouter } from "next/navigation";
-import { useSelector } from "react-redux";
+import { restartGame, selectWinner } from "@/lib/features/game";
 import { selectMyPlayerInGame } from "../../../utils/helpers";
 import { TEAM_COLOR } from "@/lib/features/game/team.types";
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ResultsTable from "@/components/resultsTable";
+import styles from "@/styles/page.module.css";
+import { useRouter } from "next/navigation";
+import { AppDispatch } from "@/lib/store";
+import { useEffect, useState } from "react";
+import { WIN_KEY } from "@/lib/features/game/game.types";
+import { useErrorModal } from "@/context/ErrorModalContext";
+import { useGamePersist } from "@/hooks/game/useGamePersist";
+import { cleanGameLocalStorage } from "@/hooks/game/useGameWsConnect";
+import { LOBBY_KEY } from "@/lib/features/lobby/lobby.types";
 
 function Results() {
+  const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();
+  const { showError } = useErrorModal();
 
   const winner = useSelector(selectWinner);
   const myPlayer = useSelector(selectMyPlayerInGame);
@@ -21,8 +29,36 @@ function Results() {
   const showModal = () => setIsModalVisible(true);
   const hideModal = () => setIsModalVisible(false);
 
-  function handleExitHome() {
+  const [lobbyId, setLobbyId] = useState<string | null>();
+
+  // Fetch again the game when the user refreshes
+  useGamePersist();
+
+  useEffect(() => {
+    // At first reload try to fetch the lobby id
+    setLobbyId(localStorage.getItem(WIN_KEY));
+  }, []);
+
+  function handleHomePage() {
+    // Reinitialize the game
+    dispatch(restartGame());
+    // Clean the persisten game from the local storage
+    // cleanGameLocalStorage(localStorage);
     router.replace("/");
+  }
+
+  function handleBackToLobby() {
+    if (!lobbyId) {
+      showError("Something went wrong. We cannot retrieve the lobby id");
+      return;
+    }
+    // Reinitialize the game
+    dispatch(restartGame());
+    // Restore the lobby id inside the preferences for the persistance
+    localStorage.setItem(LOBBY_KEY, lobbyId);
+    // Clean the persisten game from the local storage
+    cleanGameLocalStorage(localStorage);
+    router.replace(`/lobby/${lobbyId}`);
   }
 
   return (
@@ -33,19 +69,24 @@ function Results() {
       {isWinner && winner === TEAM_COLOR.RED && (
         <div className={styles.redOverlay} />
       )}
+
       <ResultsTable visible={isModalVisible} onClose={hideModal} />
+
       <div className={styles.messageContainer}>
-        {isWinner ? (
-          <div className={styles.resultsMessage}>
-            Congrats no one can outspy you!
-          </div>
-        ) : (
-          <div className={styles.resultsMessage}>Better luck next time...</div>
-        )}
+        <div className={styles.messageField}>
+          <p className={styles.resultsMessage}>
+            {isWinner
+              ? "Congrats, no one can outspy you!"
+              : "Better luck next time..."}
+          </p>
+        </div>
 
         <div className={styles.regularButtonContainer}>
-          <button className={styles.regularButton} onClick={handleExitHome}>
-            Go Home
+          <button className={styles.regularButton} onClick={handleHomePage}>
+            Home Page
+          </button>
+          <button className={styles.regularButton} onClick={handleBackToLobby}>
+            Back to Lobby
           </button>
           <button className={styles.regularButton} onClick={showModal}>
             View Results
