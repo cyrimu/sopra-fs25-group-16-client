bf889751b19f8e49878ec015b3570a5d3db2c425
sgm17 <sergigm78@gmail.com>
2025-05-17 17:06:54 +0200

    results table refactor and separate styles

--- app/components/resultsTable.tsx
+++ app/components/resultsTable.tsx
@@ -1,147 +1,67 @@
-import React from "react";
+import { selectPlayers, selectWinner } from "@/lib/features/game";
+import { TEAM_COLOR } from "@/lib/features/game/team.types";
 import styles from "@/styles/page.module.css";
-import { useRouter } from "next/navigation";
-import { Popconfirm } from "antd";
-import {
-  selectPlayers,
-  selectWinner
-} from "@/lib/features/game";
-import { selectLobbyId} from "@/lib/features/lobby";
-import { selectIsHost } from "../../utils/helpers";
 import { useSelector } from "react-redux";
+import React from "react";
+import Modal from "antd/es/modal";
 
-const ResultsTable: React.FC = () => {
-  const router = useRouter();
+interface ResultsModalProps {
+  visible: boolean;
+  onClose: () => void;
+}
 
+const ResultsTable: React.FC<ResultsModalProps> = ({ visible, onClose }) => {
   const winner = useSelector(selectWinner);
-  const winnerTeam = winner === "RED" ? "RED" : "BLUE";
-  const loserTeam = winner === "RED" ? "BLUE" : "RED";
-
-  const lobbyId = useSelector(selectLobbyId);
-
-  const isHost = useSelector(selectIsHost);
-
   const players = useSelector(selectPlayers);
 
-  const capitalize = (s: string) =>
-    s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
-
-  const confirmDeleteLobby = () => {
-    router.replace("/create");
-  };
-
-  const cancelDeleteLobby = () => {
-    // Do nothing
-  };
+  const capitalize = (s: string | undefined) =>
+    !s ? "" : s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
 
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          width: "100%",
-          marginBottom: "30px",
-        }}
-      >
+    <Modal title="Results" open={visible} onCancel={onClose}>
+      <div>
         <div
-          className={styles.messageField}
           style={{
+            display: "flex",
+            justifyContent: "space-around",
             width: "100%",
-            padding: "30px",
-            height: "auto",
-            fontSize: "26px",
-            textAlign: "center",
-            marginRight: "30px",
+            marginBottom: "30px",
           }}
         >
-          Winner: {capitalize(winnerTeam)}
+          <div className={styles.resultsTeam}>
+            Winner: {capitalize(winner ?? "")}
+          </div>
+          <div className={styles.resultsTeam}>
+            Loser:{" "}
+            {capitalize(
+              winner === TEAM_COLOR.RED ? TEAM_COLOR.BLUE : TEAM_COLOR.RED
+            )}
+          </div>
         </div>
-        <div
-          className={styles.messageField}
-          style={{
-            width: "100%",
-            padding: "30px",
-            height: "auto",
-            fontSize: "26px",
-            textAlign: "center",
-          }}
-        >
-          Loser: {capitalize(loserTeam)}
-        </div>
-      </div>
 
-      <table className={styles.tableField} style={{ minWidth: "600px" }}>
-        <thead>
-          <tr>
-            <th>codename</th>
-            <th>team</th>
-            <th>role</th>
-          </tr>
-        </thead>
-        <tbody>
-          {players
-            ?.slice()
-            .sort((a, b) => {
-              if (a.team === winner && b.team !== winner) return -1;
-              if (a.team !== winner && b.team === winner) return 1;
-              return 0;
-            })
-            .map(({ playerName, team, role }, i) => {
+        <table className={styles.tableField} style={{ minWidth: "600px" }}>
+          <thead>
+            <tr>
+              <th>codename</th>
+              <th>team</th>
+              <th>role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {players?.map(({ playerName, team, role }, i) => {
               const roleString = role?.split("_")[1];
               return (
                 <tr key={i}>
                   <td>{playerName}</td>
-                  <td>{capitalize(team ?? "")}</td>
-                  <td>{capitalize(roleString ?? "")}</td>
+                  <td>{capitalize(team)}</td>
+                  <td>{capitalize(roleString)}</td>
                 </tr>
               );
             })}
-        </tbody>
-      </table>
-
-      <div className={styles.regularButtonContainer}>
-        {isHost ? (
-          <>
-            <button
-              className={styles.regularButton}
-              onClick={() => router.push(`/lobby/${lobbyId}`)}
-            >
-              Play Again
-            </button>
-            <button
-              className={styles.regularButton}
-              onClick={() => router.push(`/lobby/${lobbyId}`)}
-            >
-              Return to Lobby
-            </button>
-            <Popconfirm
-              title={
-                <span style={{ color: "black" }}>
-                  Are you sure if you want to delete the lobby?
-                </span>
-              }
-              onConfirm={confirmDeleteLobby}
-              onCancel={cancelDeleteLobby}
-              okText="Yes"
-              cancelText="No"
-              icon={false}
-            >
-              <button className={styles.regularButton}>Delete Lobby</button>
-            </Popconfirm>
-          </>
-        ) : (
-          <>
-            <button
-              className={styles.regularButton}
-              onClick={() => router.push(`/lobby/${lobbyId}`)}
-            >
-              Exit Lobby
-            </button>
-          </>
-        )}
+          </tbody>
+        </table>
       </div>
-    </>
+    </Modal>
   );
 };
 
