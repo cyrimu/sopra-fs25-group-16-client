19fc8bf70bc29f985647c2d9431894c3ccbd4890
sgm17 <sergigm78@gmail.com>
2025-05-18 11:43:30 +0200

    remove credits from the home screen

--- app/page.tsx
+++ app/page.tsx
@@ -10,30 +10,6 @@ export default function Home() {
   return (
     <div className={styles.centered}>
       <div className={styles.redBlueOverlay} />
-      {/* Credits to the people that built the project */}
-      <div className={styles.creditWrapper}>
-        <div className={styles.creditColumn}>
-          <div className={styles.creditRow}>
-            <span className={styles.creditNames}>
-              Rashmi Dindgur (17-611-005)
-            </span>
-            <span className={styles.creditNames}>
-              Sergi Garcia (24-753-980)
-            </span>
-          </div>
-          <div className={styles.creditRow}>
-            <span className={styles.creditNames}>
-              Cyril Müller (22-700-330)
-            </span>
-            <span className={styles.creditNames}>
-              Piotr Wojtaszewski (18-621-441)
-            </span>
-          </div>
-          <div className={styles.creditRow}>
-            <span className={styles.creditNames}>Calvin Koch (20-611-539)</span>
-          </div>
-        </div>
-      </div>
       <div className={styles.homeContainer}>
         <LargeButton onClick={() => router.push("/create")}>Create</LargeButton>
         <LargeButton onClick={() => router.push("/join")}>Join</LargeButton>
